847ba7434c862e6b52e6cf25b4ae7d58b32a39d8
Plathu28 <premieromggg11@gmail.com>
2026-03-09 13:43:35 +0700

    แก้ error
    แก้ error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Project_Mobilewebapp/', // <-- เพิ่มบรรทัดนี้ลงไป (เปลี่ยนเป็นชื่อ repo ของคุณ)
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
