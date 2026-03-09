f943a32010c9ce30a838be2f708480934ec69663
Plathu28 <premieromggg11@gmail.com>
2026-03-09 13:40:33 +0700

    build ขึ้น github
    build ขึ้น github

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-});
+  base: '/Project_Mobilewebapp/', // <-- เพิ่มบรรทัดนี้ลงไป (เปลี่ยนเป็นชื่อ repo ของคุณ)
+})
